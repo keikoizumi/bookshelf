c3f0ba2c601de4a0f2f03e2f0dff270f8e4d7e3a
KOIZUMI KEITARO <dlv4148@deliv.local>
2020-03-16 22:33:03 +0900

    add

--- static/js/spa.js
+++ static/js/spa.js
@@ -4,7 +4,12 @@ var tUrl = 'http://localhost:8087/';
 var all = 'all';
 var key = 'key';
 var sflag = 0;
-var savedata;
+
+//メッセージ
+var NO_DATA = '<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">NO DATA</div></td></tr>';
+var FAILURE = '<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>';
+var RUNNING = '<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</td><td><div color: #0000FF;font-size:x-large ;font-weight: 700;">RUNNING　<img src="./static/img/ico/load.gif" width="30" height="30" /></div></td></tr>';
+var RUNNING_NOW = '<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</td><td><div color: #0000FF;font-size:x-large ;font-weight: 700;">RUNNING NOW ( PLEASE WAIT A MINUTE )　<img src="./static/img/ico/load.gif" width="30" height="30" /></div></td></tr>'; 　
 
 function today() {
   var d = new Date();
@@ -21,7 +26,6 @@ function allBooks() {
       var targetUrl = tUrl+'allBooks';
       request = 'all'
       console.log(request);
-
       $.ajax({
         url: targetUrl,
         type: 'POST',
@@ -32,35 +36,33 @@ function allBooks() {
       }).done(function(data){ 
         console.log(data);
         console.log(data.length);
-
         sflag = 0;
         $('#table').empty();
-
         if (data.length == 0) {
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">NO DATA</div></td></tr>');
+          $('#table').append(NO_DATA);
         } else {
           for(var i in data) {
             var status_img = "";
-            var rental_start_dt ="";
 
             if (data[i].rental_status == 0) {   
-              status_img = '<img class="rentalCheck" value="'+data[i].id+'" src="./static/img/ico/rental_ok.jpeg" width="17" height="17" alt="rental ok" data-toggle="modal" data-target="#rentalregistration"></img>';
+              status_img = '<img class="rentalCheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/rental_ok.jpeg" width="17" height="17" alt="rental ok" data-toggle="modal" data-target="#rentalregistration"></img>';
             } else {
-              status_img = '<img class="rentalInfocheck" value="'+data[i].id+'" src="./static/img/ico/rental_ng.jpeg" width="17" height="17" alt="rental ng" data-toggle="modal" data-target="#rentalInfo"></img>';
-            }
-            if (data[i].rental_start_dt == null) {
-              rental_start_dt = 'ー';
-            } else {
-              rental_start_dt = data[i].rental_start_dt;
+              status_img = '<img class="rentalInfocheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/rental_ng.jpeg" width="17" height="17" alt="rental ng" data-toggle="modal" data-target="#rentalInfo"></img>';
             }
 
-            $('#table').append('<tr><td>'+data[i].area+'</td><td>'+data[i].title+'</td><td>'+status_img+'</td></tr>');
+            if (data[i].url == "" || data[i].url == null) {
+              link_img = '<div><img class="rentalInfocheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/dead_link.jpeg" width="17" height="17" alt="dead link"></img>';
+            } else {
+              link_img = '<a href="data[i].url"><img class="rentalInfocheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/link.jpeg" width="17" height="17" alt="link"></img>';
+            }
+
+            $('#table').append('<tr><td>'+data[i].area+'</td><td>'+data[i].title+'</td><td>'+status_img+'</td><td>'+link_img+'</td></tr>');
           }
         }
         }).fail(function(data, XMLHttpRequest, textStatus) {
           console.log(data);
           $('#table').empty();
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>');
+          $('#table').append(FAILURE);
           sflag = 0;
           console.log("XMLHttpRequest : " + XMLHttpRequest.status);
           console.log("textStatus     : " + textStatus);
@@ -71,13 +73,10 @@ function allBooks() {
 function searchBook(data) {  
   $(function(){
       var targetUrl = tUrl+'searchBook';
-
       console.log(data);
-
       var request = {
         'data': data
       };
-
       $.ajax({
         url: targetUrl,
         type: 'POST',
@@ -88,36 +87,33 @@ function searchBook(data) {
       }).done(function(data){ 
           console.log(data);
           console.log(data.length);
-
           sflag = 0;
           $('#table').empty();
-
           if (data.length == 0) {
-            $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">NO DATA</div></td></tr>');
+            $('#table').append(NO_DATA);
           } else {
             for(var i in data) {
               var status_img = "";
-              var rental_start_dt ="";
+              var link_img = "";
 
               if (data[i].rental_status == 0) {   
-                status_img = '<img class="rentalCheck" value="'+data[i].id+'" src="./static/img/ico/rental_ok.jpeg" width="17" height="17" alt="rental ok" data-toggle="modal" data-target="#rentalregistration"></img>';
+                status_img = '<img class="rentalCheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/rental_ok.jpeg" width="17" height="17" alt="rental ok" data-toggle="modal" data-target="#rentalregistration"></img>';
               } else {
-                status_img = '<img class="rentalInfocheck" value="'+data[i].id+'" src="./static/img/ico/rental_ng.jpeg" width="17" height="17" alt="rental ng" data-toggle="modal" data-target="#rentalInfo"></img>';
+                status_img = '<img class="rentalInfocheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/rental_ng.jpeg" width="17" height="17" alt="rental ng" data-toggle="modal" data-target="#rentalInfo"></img>';
               }
-              if (data[i].rental_start_dt == null) {
-                rental_start_dt = 'ー';
+              if (data[i].url == "" || data[i].url == null) {
+                link_img = '<div><img class="rentalInfocheck" value="'+data[i].id+'" src="./static/img/ico/dead_link.jpeg" width="17" height="17" alt="dead link"></img>';
               } else {
-                rental_start_dt = data[i].rental_start_dt;
+                link_img = '<img class="rentalInfocheck cursorhand" value="'+data[i].id+'" src="./static/img/ico/link.jpeg" width="17" height="17" alt="link"></img>';
               }
 
-              $('#table').append('<tr><td>'+data[i].area+'</td><td class="highlight">'+data[i].title+'</td><td>'+status_img+'</td></tr>');
+              $('#table').append('<tr><td>'+data[i].area+'</td><td>'+data[i].title+'</td><td>'+status_img+'</td><td>'+link_img+'</td></tr>');
             }
           }
-      
         }).fail(function(data, XMLHttpRequest, textStatus) {
           console.log(data);
           $('#table').empty();
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>');
+          $('#table').append(FAILURE);
           sflag = 0;
           console.log("XMLHttpRequest : " + XMLHttpRequest.status);
           console.log("textStatus     : " + textStatus);
@@ -130,14 +126,10 @@ function rentalInfo(data) {
   $(function(){
       var targetUrl = tUrl+'rentalInfo';
       console.log(data);
-
-
       var request = {
         'data': data
       };
-
       console.log(data);
-
       $.ajax({
         url: targetUrl,
         type: 'POST',
@@ -148,14 +140,12 @@ function rentalInfo(data) {
       }).done(function(data){ 
           console.log(data);
           console.log(data.length);
-
           sflag = 0;
           $('#rental_user_name').empty();
           $('#rental_start_dt').empty();
           $('#rental_end_plan_dt').empty();
-
           if (data.length == 0) {
-            $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">NO DATA</div></td></tr>');
+            $('#table').append(NO_DATA);
           } else {
 
             if (data[0].rental_user_name =="" || data[0].rental_user_name == null) {
@@ -181,7 +171,7 @@ function rentalInfo(data) {
         }).fail(function(data, XMLHttpRequest, textStatus) {
           console.log(data);
           $('#table').empty();
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>');
+          $('#table').append(FAILURE);
           sflag = 0;
           console.log("XMLHttpRequest : " + XMLHttpRequest.status);
           console.log("textStatus     : " + textStatus);
@@ -193,7 +183,6 @@ function rentalInfo(data) {
 function rentalBook(request) {  
   $(function(){
       var targetUrl = tUrl+'rentalBook';
-
       $.ajax({
         url: targetUrl,
         type: 'POST',
@@ -204,10 +193,11 @@ function rentalBook(request) {
       }).done(function(data){ 
           console.log(data);
           allBooks()
+          sflag = 0;
         }).fail(function(data, XMLHttpRequest, textStatus) {
           console.log(data);
           $('#table').empty();
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>');
+          $('#table').append(FAILURE);
           sflag = 0;
           console.log("XMLHttpRequest : " + XMLHttpRequest.status);
           console.log("textStatus     : " + textStatus);
@@ -219,7 +209,6 @@ function rentalBook(request) {
 function returnBook(request) {  
   $(function(){
       var targetUrl = tUrl+'returnBook';
-
       $.ajax({
         url: targetUrl,
         type: 'POST',
@@ -230,10 +219,11 @@ function returnBook(request) {
       }).done(function(data){ 
           console.log(data);
           allBooks()
+          sflag = 0;
         }).fail(function(data, XMLHttpRequest, textStatus) {
           console.log(data);
           $('#table').empty();
-          $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</div></td><td><div style="color: #FF3300;font-size:x-large ;font-weight: 700;">FAILURE</div></td></tr>');
+          $('#table').append(FAILURE);
           sflag = 0;
           console.log("XMLHttpRequest : " + XMLHttpRequest.status);
           console.log("textStatus     : " + textStatus);
@@ -249,30 +239,24 @@ $(function(){
       data= $("#key").val();
       console.log('data');
       searchBook(data);
-
       $('#table').empty();
       $('#iimg').empty();
-      $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</td><td><div color: #0000FF;font-size:x-large ;font-weight: 700;">RUNNING　<img src="./static/img/ico/load.gif" width="30" height="30" /></div></td></tr>');
+      $('#table').append(RUNNING);
     } else {
-      console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbb");
       $('#table').empty();
       $('#iimg').empty();
-      $('#table').append('<tr><td><div style="color: #000000;font-size:x-large ;font-weight: 700;">INFO</td><td><div color: #0000FF;font-size:x-large ;font-weight: 700;">RUNNING NOW ( PLEASE WAIT A MINUTE )　<img src="./static/img/ico/load.gif" width="30" height="30" /></div></td></tr>');
+      $('#table').append(RUNNING_NOW);
     }  
   });
 });
 
-
 //rentalCheck
 $(function() {
   $(document).on('click','.rentalCheck',function() {
-
     sflag = 1;
     id =  $(this).attr("value");
-
     console.log('data');
     console.log(id);
-
     $('.rentalBook').val(id);
 
   });
@@ -281,61 +265,43 @@ $(function() {
 //rentalBook
 $(function(){ 
   $(document).on('click','.rentalBook',function() {
-    sflag == 0
-
       sflag = 1;
       console.log("click")
       id = $(".rentalBook").val();
       InputEmail1 = $("#InputEmail1").val();
       inputDate = $("#inputDate").val();
-
       var request = {
         'id': id
         , 'InputEmail1': InputEmail1
         , 'inputDate': inputDate
       };
-      
       console.log(request);
-      
       rentalBook(request);
-
-
   });
 });
 
 //rentalInfo
 $(function() {
   $(document).on('click','.rentalInfocheck',function() {
-
     sflag = 1;
     id =  $(this).attr("value");
-
     console.log('data');
     console.log(id);
     rentalInfo(id);
-
     $('.returnBook').val(id);
-
   });
 });
-
 
 //returnBook
 $(function(){ 
   $(document).on('click','.returnBook',function() {
-    sflag == 0
- 
       sflag = 1;
       console.log("click")
       id = $(".returnBook").val();
-
       var request = {
         'id': id
       };
-      
       console.log(request);
-      
       returnBook(request);
- 
   });
 });
